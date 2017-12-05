462d50483c986cdc91fd0f66125e28f90a46f0dd
Aaron Lam <aaron.chihon.lam@gmail.com>
2017-12-04 22:15:35 -0500

    Chapter 4 example code

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,46 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
+  def new
+  end
+
+  def create
+    @movie = Movie.create!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    id = params[:id]
+    @movie = Movie.find(id)
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes!(movie_params)
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    respond_to do |client_wants|
+      client_wants.html { redirect_to movies_path }
+      client_wants.xml { render :xml => @movie.xml }
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "#{@movie.title} was successfully deleted."
+    redirect_to movies_path
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :release_date)
+  end
+end
